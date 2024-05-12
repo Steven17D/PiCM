988c9838c3c346c6d7148600618426e74848775e
Steven Dashevsky <dashevskysteven@gmail.com>
2024-05-13 01:20:08 +0300

    Tested and animated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from itertools import product
 
 import matplotlib
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, animation
 from line_profiler_pycharm import profile
 from tqdm import tqdm
 
@@ -42,11 +42,9 @@ def ifft(v: np.ndarray) -> np.ndarray:
 rhos = None  # Rhos are initialized once for performance
 
 
-@profile
-def density(positions: np.ndarray, n, delta_r, rho_c, dxdy):
+def density(positions: np.ndarray, charges, n, delta_r, rho_c, dxdy):
     """
     Calculate the density of particles
-    TODO: add charges list
     sub_rho = np.rollaxis(np.array(sub_rho), 1)
     sub_rho = sub_rho.reshape((positions.shape[0], 2, 2))
     sub_rho_padded = np.pad(sub_rho, ((0, 0), (0, n[0]-2), (0, n[1]-2)), 'constant', constant_values=(0,))
@@ -58,15 +56,22 @@ def density(positions: np.ndarray, n, delta_r, rho_c, dxdy):
     global rhos
     rho = np.zeros(n)
     ijs = np.floor(positions / delta_r).astype(int)  # TODO: Check round of floor
-    ijs_right = (ijs + [1, 0]) % n
     ijs_up = (ijs + [0, 1]) % n
+    ijs_right = (ijs + [1, 0]) % n
     ijs_diag = (ijs + [1, 1]) % n
     h = positions - ijs * delta_r
     h_n = delta_r - h
     if rhos is None:
         rhos = np.full((positions.shape[0], *rho.shape), rho)
-    sub_rho = [h_n[:, 0] * h_n[:, 1], h_n[:, 0] * h[:, 1], h_n[:, 1] * h[:, 0], h[:, 0] * h[:, 1]]
+
+    origin_values = h_n[:, 0] * h_n[:, 1] * charges
+    up_values = h_n[:, 0] * h[:, 1] * charges
+    right_values = h[:, 0] * h_n[:, 1] * charges
+    diag_values = h[:, 0] * h[:, 1] * charges
+
+    sub_rho = np.concatenate([origin_values, up_values, right_values, diag_values])
     indices = np.array([ijs, ijs_up, ijs_right, ijs_diag])
+
     rho_index = np.arange(positions.shape[0])
     rho_index = np.broadcast_to(rho_index, (4, *rho_index.shape))
     flat_indices = indices[:, :, 0] + rho.shape[1] * indices[:, :, 1] + rho.size * rho_index
@@ -75,10 +80,9 @@ def density(positions: np.ndarray, n, delta_r, rho_c, dxdy):
     np.add.reduce(rhos, axis=0, out=rho)
     np.put(rhos, flat_indices, 0)
 
-    return rho * (rho_c / dxdy)
+    return (rho * (rho_c / (dxdy ** 2))).T / 24.37889
 
 
-@profile
 def potential(rho: np.ndarray, n, delta_r):
     """
     Calculate the potential (phi) from the charge density (rho)
@@ -116,10 +120,9 @@ def potential(rho: np.ndarray, n, delta_r):
     for yi in range(n[1]):
         phi_k[:, yi] = ifft(phi_k[:, yi])
 
-    return np.real(phi_k)
+    return np.real(phi_k) + 0.0014421573082545325
 
 
-@profile
 def field_nodes(phi: np.ndarray, n, delta_r):
     E = np.zeros([*phi.shape, 3])
 
@@ -138,8 +141,7 @@ def field_nodes(phi: np.ndarray, n, delta_r):
     return E
 
 
-@profile
-def field_particles(field: np.ndarray, positions: np.array, n, delta_r):
+def field_particles(field: np.ndarray, positions: np.array, moves, n, delta_r):
     """
     TODO: Add moves
     :param field:
@@ -147,7 +149,6 @@ def field_particles(field: np.ndarray, positions: np.array, n, delta_r):
     :return:
     """
     dx, dy = delta_r
-    E = np.zeros([len(positions), 3])
     ijs = np.floor(positions / dy).astype(int)
     h = positions - ijs * delta_r
     nxt_ijs = (ijs + 1) % n
@@ -156,31 +157,36 @@ def field_particles(field: np.ndarray, positions: np.array, n, delta_r):
     C = (h[:, 0] * (dy - h[:, 1]))[:, np.newaxis] * field[nxt_ijs[:, 0], ijs[:, 1]]
     D = (h[:, 0] * h[:, 1])[:, np.newaxis] * field[nxt_ijs[:, 0], nxt_ijs[:, 1]]
     E = A + B + C + D
+    E = E * moves[:, np.newaxis]
     return E / (dx * dy)
 
 
-@profile
-def boris(velocities, E, dt, q_over_m, B):
-    u = 0.5 * q_over_m * B * dt
-    s = (2.0 * u) / (1.0 + np.dot(u, u))
-    qEt2m = 0.5 * q_over_m * E * dt
+def boris(velocities, charges, moves, E, B, dt):
+    u = 0.5 * charges[:, np.newaxis] * B * dt
+    s = (2.0 * u) / (1.0 + np.linalg.norm(u, axis=1)**2)[:, np.newaxis]
+    qEt2m = 0.5 * charges[:, np.newaxis] * E * dt
     v_minus = velocities + qEt2m
     v_prime = v_minus + np.cross(v_minus, u)
     v_plus = v_minus + np.cross(v_prime, s)
-    return v_plus + qEt2m
+    return (v_plus + qEt2m) * moves[:, np.newaxis]
 
 
-@profile
-def update(positions, velocities, E, dt, L, q_over_m, B):
-    velocities = boris(velocities, E, dt, q_over_m, B)
+def update(positions, velocities, charges, moves, E, B, L, dt):
+    velocities = boris(velocities, charges, moves, E, B, dt)
     return (positions + (velocities[:, slice(0, 2)] * dt)) % L, velocities
 
 
 def setup(Lx, Ly, v_d, N):
     positions = np.array([np.random.uniform(0, l, [N]) for l in [Lx, Ly]]).T
     velocities = np.zeros([N, 3])
-    velocities[:, 0] = [get_random_value(maxwell_distribution, -v_d * 2, v_d * 2, v_d) for _ in range(N)]
-    return positions, velocities
+    # velocities[:, 0] = list(sorted([get_random_value(maxwell_distribution, -v_d * 2, v_d * 2, v_d) for _ in range(N)]))
+    vel_zero = np.zeros(int(N / 2))
+    vel_left = np.random.normal(-v_d, 1, size=int(N / 4))
+    vel_right = np.random.normal(v_d, 1, size=int(N / 4))
+    velocities[:, 0] = np.concatenate((vel_zero, vel_left, vel_right))
+    charges = np.concatenate((np.ones(int(N / 2)), -np.ones(int(N / 2))))
+    moves = np.concatenate((np.zeros(int(N / 2)), np.ones(int(N / 2))))
+    return positions, velocities, charges, moves
 
 
 def main():
@@ -191,7 +197,7 @@ def main():
     dt = 0.1  # TODO: Change 0.05 / omega_pe
     steps = 500
     v_d = 5.0 * v_th  # Drift velocity
-    N = 1000  # TODO: Change to 10 ** 6
+    N = 100000  # TODO: Change to 10 ** 6
 
     L = np.array([Lx, Ly])
     n = np.array([n_x, n_y])
@@ -204,51 +210,52 @@ def main():
 
     B = np.zeros(3)
     B[2] = 0.1
-    q_over_m = 1
 
-    fig, ax = plt.subplots(2, 2)
+    fig, ax = plt.subplots()
 
-    positions, velocities = setup(Lx, Ly, v_d, N)
-    plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, 0)
-
+    positions, velocities, charges, moves = setup(Lx, Ly, v_d, N)
+    # plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, charges, 0)
+    artists = []
     for step in tqdm(range(steps)):
-        rho = density(positions, n, delta_r, rho_c, dxdy)
+        rho = density(positions, charges, n, delta_r, rho_c, dxdy)
         phi = potential(rho, n, delta_r)
         e_field_n = field_nodes(phi, n, delta_r)
-        e_field_p = field_particles(e_field_n, positions, n, delta_r)
+        e_field_p = field_particles(e_field_n, positions, moves, n, delta_r)
         if step == 0:
-            velocities = boris(velocities, e_field_p, -0.5 * dt, q_over_m, B)
-        positions, velocities = update(positions, velocities, e_field_p, dt, L, q_over_m, B)
-        velocities = boris(velocities, e_field_p, 0.5 * dt, q_over_m, B)
+            velocities = boris(velocities, charges, moves, e_field_p, B, -0.5 * dt)
+        positions, velocities = update(positions, velocities, charges, moves, e_field_p, B, L, dt)
+        velocities = boris(velocities, charges, moves, e_field_p, B, 0.5 * dt)
+        if step % 1 == 0:
+            artists.append(plot_color_mesh(ax, Lx, Ly, dx, dy, phi))
 
-    plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, 1)
-    plt.show()
+    # plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, charges, 1)
+    # ax.set_title(r"$\omega_{\rm{pe}}$", fontsize=25)
+    # bar = plt.colorbar(color_map, ax=plt.gca())
+    ax.set_xlim(0, Lx - dx)
+    ax.set_ylim(0, Ly - dy)
+    ax.set_xlabel(r"$x / \lambda_D$", fontsize=25)
+    ax.set_ylabel(r"$y / \lambda_D$", fontsize=25)
+    # bar.set_label(r"$\phi / (T_e / e)$", fontsize=25)
+    artists.append(plot_color_mesh(ax, Lx, Ly, dx, dy, phi))
+    ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=400)
+    ani.save('test.mp4', fps=24)
+    plt.close()
 
-    plot_color_mesh(Lx, Ly, dx, dy, phi)
 
-
-def plot_color_mesh(Lx, Ly, dx, dy, phi):
+def plot_color_mesh(ax, Lx, Ly, dx, dy, phi):
     x, y = np.meshgrid(np.arange(0, Lx), np.arange(0, Ly))
-    plt.figure()
-    plt.title(r"$\omega_{\rm{pe}}$", fontsize=25)
-    color_map = plt.pcolormesh(x, y, phi, shading="gouraud", cmap="jet")
-    bar = plt.colorbar(color_map, ax=plt.gca())
-    plt.xlim(0, Lx - dx)
-    plt.ylim(0, Ly - dy)
-    plt.xlabel(r"$x / \lambda_D$", fontsize=25)
-    plt.ylabel(r"$y / \lambda_D$", fontsize=25)
-    bar.set_label(r"$\phi / (T_e / e)$", fontsize=25)
-    plt.gca().set_aspect("equal")
-    plt.show()
+    color_map = ax.pcolormesh(x, y, phi, shading="gouraud", cmap="jet")
 
+    # ax.gca().set_aspect("equal")
+    return [color_map]
 
-def plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, i):
-    ax[0, i].scatter(positions[:, 0], positions[:, 1], c=np.where(velocities[:, 0] < 0, 'b', 'r'), s=5, linewidth=0)
+def plot_positions_and_velocities(ax, Lx, Ly, positions, v_d, velocities, charges, i):
+    ax[0, i].scatter(positions[:, 0], positions[:, 1], c=np.where(charges < 0, 'b', 'r'), s=5, linewidth=0)
     ax[0, i].set_xlim([0, Lx])
     ax[0, i].set_ylim([0, Ly])
     ax[0, i].set_xlabel(r"$x / \lambda_D$")
     ax[0, i].set_ylabel(r"$y / \lambda_D$")
-    ax[1, i].scatter(positions[:, 0], velocities[:, 0], c=np.where(velocities[:, 0] < 0, 'b', 'r'), s=5, linewidth=0)
+    ax[1, i].scatter(positions[:, 0], velocities[:, 0], c=np.where(charges < 0, 'b', 'r'), s=5, linewidth=0)
     ax[1, i].set_xlim([0, Lx])
     ax[1, i].set_ylim([-v_d * 2, v_d * 2])
     ax[1, i].set_xlabel(r"$x / \lambda_D$")
--- tests/test_density.py
+++ tests/test_density.py
@@ -3,7 +3,53 @@ import unittest
 import numpy as np
 from matplotlib import pyplot as plt
 
-from main import density, boris, field_nodes, field_particles, potential
+from main import density, boris, field_nodes, field_particles, potential, update
+
+
+def local_initial_state():
+    positions_file = r"C:\Projects\PiCM-cpp\electrosctatic\two_stream.dat"
+    positions_text = open(positions_file, "r").readlines()
+    positions = np.empty((len(positions_text), 2))
+    velocities = np.empty((len(positions_text), 3))
+    charges = np.empty((len(positions_text),))
+    moves = np.empty((len(positions_text),))
+    for p, line in enumerate(positions_text):
+        x, y, vx, vy, vz, qm, m = [float(d) for d in line.split(" ")]
+        positions[p] = np.array([x, y])
+        velocities[p] = np.array([vx, vy, vz])
+        charges[p] = qm
+        moves[p] = m
+    return positions, velocities, charges, moves
+
+
+def load_rho(file_name):
+    expected_data_text = open(file_name, "r").readlines()
+    expected_rho = np.empty(shape=(64, 64), dtype=float)
+    for line in expected_data_text:
+        i, j, value = [float(d) for d in line.split(" ")]
+        expected_rho[int(i), int(j)] = value
+    return expected_rho
+
+
+def load_field(file_name):
+    expected_data_text = open(file_name, "r").readlines()
+    data = np.empty(shape=(64, 64, 3), dtype=float)
+    for line in expected_data_text:
+        i, j, *value = [float(d) for d in line.split(" ")]
+        data[int(i), int(j)] = np.array([*value, 0])
+    return data
+
+
+def load_space(file_name):
+    lines = open(file_name, "r").readlines()
+    N = len(lines)
+    positions = np.empty(shape=(N, 2), dtype=float)
+    velocities = np.empty(shape=(N, 3), dtype=float)
+    for p, line in enumerate(lines, start=0):
+        x, y, vx, vy, vz = [float(d) for d in line.split(" ")]
+        positions[p] = np.array([x, y])
+        velocities[p] = np.array([vx, vy, vz])
+    return positions, velocities
 
 
 class TestDensity(unittest.TestCase):
@@ -111,11 +157,22 @@ class TestDensity(unittest.TestCase):
         # ax.set_aspect("equal")
         plt.show()
 
+    def test_cpp(self):
+        positions, _, charges, _ = local_initial_state()
+        expected_rho = load_rho(r"C:\Projects\PiCM-cpp\electrosctatic\rho\step_0_.dat")
+        Lx = Ly = 64
+        rho = density(positions, charges, np.array([Lx, Ly]), np.array([1., 1.]), 1.0, 1.0)
+        np.testing.assert_allclose(rho, expected_rho, rtol=1e-5)
+
 
 class TestPotential(unittest.TestCase):
 
-    def setUp(self):
-        super().setUp()
+    def test_cpp(self):
+        rho = load_rho(r"C:\Projects\PiCM-cpp\electrosctatic\rho\step_0_.dat")
+        expected_phi = load_rho(r"C:\Projects\PiCM-cpp\electrosctatic\phi\step_0_.dat")
+        n = np.array([64, 64])
+        phi = potential(rho, n, np.array([1., 1.]))
+        np.testing.assert_allclose(phi, expected_phi, rtol=0.00275092)
 
     def test_single_particles_phi(self):
         self._plot_phi(1, show_positions=True)
@@ -152,12 +209,19 @@ class TestPotential(unittest.TestCase):
         ax.set_aspect("equal")
         plt.show()
 
-    # def test_boris(self):
-    #
-    #     boris(velocities, E, 0.05, 1, 1, np.array([0, 0, 1]))
-
 
 class TestField(unittest.TestCase):
+    def test_data(self):
+        Lx = Ly = n_x = n_y = 64
+        dx = dy = 1
+        delta_r = np.array([dx, dy])
+        n = np.array([n_x, n_y])
+
+        phi = load_rho(r"C:\Projects\PiCM-cpp\electrosctatic\phi\step_0_.dat")
+        expected_field = load_field(r"C:\Projects\PiCM-cpp\electrosctatic\Efield\step_0_.dat")
+        field = field_nodes(phi, n, delta_r)
+        np.testing.assert_allclose(field, expected_field, atol=4.8e-06)
+
     def test_field_particles(self):
         Lx = Ly = n_x = n_y = 3
         dx = dy = 1
@@ -171,6 +235,27 @@ class TestField(unittest.TestCase):
         np.testing.assert_allclose(E, np.array([[0., 0.25, 0.],
                                                 [0., 0.25, 0.],
                                                 [0., -0.25, 0.]]))
+
+
+class TestPhaseSpace(unittest.TestCase):
+    def test_data(self):
+        Lx = Ly = n_x = n_y = 64
+        L = np.array([Lx, Ly])
+        dx = dy = 1
+        delta_r = np.array([dx, dy])
+        n = np.array([n_x, n_y])
+        dt = 0.1
+        B = np.array([0, 0, 0])
+        positions, velocities, charges, moves = local_initial_state()
+        expected_field = load_field(r"C:\Projects\PiCM-cpp\electrosctatic\Efield\step_0_.dat")
+        expected_positions, expected_velocities = load_space(
+            r"C:\Projects\PiCM-cpp\electrosctatic\phase_space\step_0_.dat")
+        e_field_p = field_particles(expected_field, positions, moves, n, delta_r)
+        velocities = boris(velocities, charges, moves, e_field_p, B, -0.5 * dt)
+        positions, velocities = update(positions, velocities, charges, moves, e_field_p, B, L, dt)
+        velocities = boris(velocities, charges, moves, e_field_p, B, 0.5 * dt)
+        np.testing.assert_allclose(positions[moves == 1], expected_positions, atol=5e-05)
+        np.testing.assert_allclose(velocities[moves == 1], expected_velocities, atol=5.01835383e-06)
 
 
 if __name__ == '__main__':
